050730dc111d3b0bdb79043be7e3dc17c273f43c
Sun Lee <sunlee334@gmail.com>
2022-09-19 00:58:35 +0900

    add dashboard

---
+++ backdata.py
@@ -0,0 +1,21 @@
+tickers = [
+    'AAPL',  # 애플
+    'AMT',  # 아메리칸 타워
+    'AMZN',  # 아마존닷컴
+    'CMG',  # 치포틀레 멕시칸 그릴
+    'EQIX',  # 에퀴닉스
+    'MSFT',  # 마이크로 소프트
+    # 'SCHD', # SCHWAB US DIVIDEND EQUITY ETF
+    'UNH'  # 유나이티드헬스 그룹
+    # 'VOO', # VANGUARD S&P 500 ETF
+    # 'VTI' # VANGUARD TOTAL STOCK MARKET ETF
+]
+investment = 100
+start_year = 2012
+start_month = 1
+start_day = 1
+end_year = 2022
+end_month = 1
+end_day = 1
+
+#%%
